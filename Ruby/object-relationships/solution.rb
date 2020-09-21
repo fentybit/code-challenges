bbe3bd44233605939c9db06b24579c8b4fa880d0
allisonkadel <akadel01@gmail.com>
2020-09-21 11:54:08 -0400

    Add Ruby code challenges

---
+++ Ruby/object-relationships/solution.rb
@@ -0,0 +1,22 @@
+class Listing
+    attr_reader :location
+    attr_accessor :status, :price
+
+    @@all = []
+
+    def initialize(location, price, status="for sale")
+        @location = location
+        @price = price
+        @status = status
+        @@all << self
+    end
+
+    def price_drop(percentage)
+        self.price -= self.price*percentage
+    end
+
+    def self.all
+        @@all
+    end
+
+end
